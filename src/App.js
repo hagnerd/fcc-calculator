bf505300dcccb3eabccff9bdfa1c225b3f886010
Matt Hagner <web@matthagner.com>
2018-07-19 09:22:47 -0500

    Complete application details

--- src/App.js
+++ src/App.js
@@ -1,9 +1,89 @@
-import React, { Component } from "react";
+import React from "react";
+import { css, injectGlobal } from "emotion";
+import styled from "react-emotion";
+import Calculator from "./containers/calculator-app";
+import {
+  NumBtns,
+  OperatorBtns,
+  EqualsBtn,
+  DecimalBtn,
+  ClearBtn
+} from "./components/button";
+import Display from "./components/display";
+import Footer from "./components/footer";
 
-class App extends Component {
-  render() {
-    return <div>App</div>;
+injectGlobal`
+  @import url('https://fonts.googleapis.com/css?family=IBM+Plex+Sans:700');
+  body, html {
+    height: 100vh;
+    width: 100vw;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    background-color: #F5F7F9;
   }
-}
+`;
+
+const Center = styled("div")`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Grid = styled("div")`
+  height: 60vh;
+  width: 50%;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: 2fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  grid-template-areas:
+    "display display display display"
+    "clear clear divide multiply"
+    "seven eight nine subtract"
+    "four five six add"
+    "one two three equals"
+    "zero zero decimal equals"
+    "error error error error";
+`;
+
+const App = () => (
+  <React.Fragment>
+    <Calculator
+      render={({
+        error,
+        displayText,
+        handleClick,
+        handleReset,
+        handleCalculate
+      }) => (
+        <Center>
+          <Grid>
+            <Display displayText={displayText} />
+            <NumBtns handleClick={handleClick} />
+            <OperatorBtns handleClick={handleClick} />
+            <EqualsBtn handleClick={() => handleCalculate(displayText)} />
+            <DecimalBtn
+              handleClick={() => handleClick({ type: "decimal", text: "." })}
+            />
+            <ClearBtn handleClick={handleReset} />
+            {error && (
+              <div
+                className={css`
+                  grid-area: error;
+                  color: #cd4045;
+                  font-family: "IBM Plex Sans", sans-serif;
+                  font-size: 1.2rem;
+                `}
+              >
+                {error}
+              </div>
+            )}
+          </Grid>
+        </Center>
+      )}
+    />
+    <Footer />
+  </React.Fragment>
+);
 
 export default App;
--- src/components/display.js
+++ src/components/display.js
@@ -1,7 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { css } from "emotion";
+import styled from "react-emotion";
 
-const Display = ({ displayText }) => <div>{displayText}</div>;
+const H2 = styled("h2")`
+  align-self: center;
+  color: white;
+  font-family: "IBM Plex Sans", sans-serif;
+  font-size: 2.5rem;
+  margin: 0;
+  padding: 20px;
+`;
+
+const display = css`
+  grid-area: display;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  background: radial-gradient(#19c75d, #10ba3f, #0fa911);
+`;
+
+const Display = ({ displayText }) => (
+  <div className={display}>
+    <H2>{displayText}</H2>
+  </div>
+);
 
 Display.propTypes = {
   displayText: PropTypes.string.isRequired
